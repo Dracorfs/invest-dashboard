4fa6325964b6cc4054b5097aacdd31aea98419d2
Dracorfs <franciscosantopietro@hotmail.com>
2023-11-13 19:27:48 -0300

    DELETE: boilerplate type comment

--- src/components/columns.tsx
+++ src/components/columns.tsx
@@ -2,8 +2,6 @@
 
 import type { ColumnDef } from "@tanstack/react-table"
 
-// This type is used to define the shape of our data.
-// You can use a Zod schema here if you want.
 type UnitPrice = {
   initial: number,
   current: number
